34b46d58946778aeb9a0f97e09e74ceb0c04a6c6
Mayank Kumar <mayankkumarverma306@gmail.com>
2025-05-06 17:35:12 +0530

    commit

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -20,7 +20,7 @@ export default function WeatherApp(){
 
     return(
         <div  style={{textAlign:"center"}}>
-            <h1>Weather App by Mayank</h1>
+            <h1>Weather App </h1>
             <SearchBox updateInfo={updateInfo} />
             <InfoBox info={weatherInfo}/>
         </div>
